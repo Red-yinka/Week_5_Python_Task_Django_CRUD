82aa23b26a6b7877a99b1e78ad3fec22545ba437
Olayinka JOHN <bisoye.jy@gmail.com>
2022-10-28 10:26:32 +0100

    Django CRUD

---
+++ songcrud/musicapp/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from email.policy import default
+from django.db import models
+
+# Create your models here.
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+#     pass
+
+    def __str__(self):
+        return self.first_name
+
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=400)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.CharField(max_length=400)
+#     pass
+
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=2000)
+    song_id = models.IntegerField()
+#     pass
+
+    def __str__(self):
+        return self.content
